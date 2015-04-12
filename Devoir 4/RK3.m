270b12bf844e777e51fd38ef78bb94bd6fadee38
Zangdaarr <Zangdaarr@localhost>
2015-04-12 14:57:06 -0400

    Premi?re tentative

--- Devoir 4/RK3.m
+++ Devoir 4/RK3.m
@@ -2,39 +2,40 @@ function [ oX, oY, oT ] = RK3(iCr, iBrs, iX0, iInput, iSystem, iNmax, iSampleTim
 
 wMatlabIndexBias = 1;
 
-
 wA = iSystem{1};
 wB = iSystem{2};
 wC = iSystem{3};
 wD = iSystem{4};
 
 oX = zeros(3,1);
-oX(:,1) = X0;
+oX(:,1) = iX0;
 
-%Compute Ar:
-wAr = sum(iBrs,2);
-
-for n = 0:iNmax-1
+for n = 0:iNmax-2
     
-    wK(1)  = wA*oX(:,n+wMatlabIndexBias) + wB*iInput(n+wMatlabIndexBias);
+    wK(:,1)  = wA*oX(:,n+wMatlabIndexBias) + wB*iInput(n+wMatlabIndexBias);
         
-    wXp(1) = oX(:,n+wMatlabIndexBias) + iSampleTime(sum(iBrs(2,1:1).*wK(1:1)));    
-    wUp(1) = iInput(n+wMatlabIndexBias);
+    wXp(:,1) = oX(:,n+wMatlabIndexBias) + iSampleTime*(sum(iBrs(1,1:1).*wK(1:1)));    
+    wUp(:,1) = iInput(n+wMatlabIndexBias);
     
-    wK(2)  = wA*wXp(1) + wB*wUp(1);
+    wK(:,2)  = wA*wXp(:,1) + wB*wUp(:,1);
 
-    wXp(2) = oX(:,n+wMatlabIndexBias) + iSampleTime(sum(iBrs(3,1:2).*wK(1:2)));
-    wUp(2) = iInput(n+wMatlabIndexBias);
+    wXp(:,2) = oX(:,n+wMatlabIndexBias) + iSampleTime*(sum(iBrs(2,1:2).*wK(1:2)));
+    wUp(:,2) = iInput(n+wMatlabIndexBias);
     
-    wK(3) = wA*wXp(2) + wB*wUp(2);
+    wK(:,3) = wA*wXp(:,2) + wB*wUp(:,2);
     
-    oX(:,n+wMatlabIndexBias+1) = oX(:,n+wMatlabIndexBias) + 1/sum(wAr)*iSampleTime*(sum(wAr.*wK));
+    wKsum = zeros(size(wK,1),1);
+    for h=1:size(wK,1)
+        wKsum(h,1) = sum(iCr(:).*wK(:,h));
+    end
+    
+    oX(:,n+wMatlabIndexBias+1) = oX(:,n+wMatlabIndexBias) + 1/sum(iCr)*iSampleTime.*wKsum;
     
 end
 
 oY = wC*oX + wD*iInput;
 
-oT=linspace(0,iSimulationTime,iSimulationTime*(1/iSampleTime));
+oT=linspace(0,iNmax*iSampleTime,iNmax);
 
 end
 
