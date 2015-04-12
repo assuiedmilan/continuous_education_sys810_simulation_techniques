be2bb766f0fd16fb38e930e4569ad6b9a7b58ce6
Zangdaarr <Zangdaarr@localhost>
2015-04-12 18:22:09 -0400

    Fix algo to be evaluating Xp correctly at each intermediate pace.

--- Devoir 4/RK3.m
+++ Devoir 4/RK3.m
@@ -7,9 +7,6 @@ wB = iSystem{2};
 wC = iSystem{3};
 wD = iSystem{4};
 
-oX = zeros(3,1);
-oX(:,1) = iX0;
-
 %Adding first rank of zeros for Brs coefficients if not already specified
 %by user
 if (all(iBrs(1,:)==0))
@@ -18,31 +15,41 @@ else
     wBrs = [zeros(1,size(iBrs,1));iBrs];
 end
 
+%Computing A(r)
+wAr = sum(wBrs,2);
+
+%Initialize
 wRKNumberOfIterations = length(iCr);
+
+wXp = zeros(wRKNumberOfIterations);
+wUp = zeros(1,wRKNumberOfIterations);
+oX = zeros(3,iNmax);
+oX(:,1) = iX0;
+
 for n = 0:iNmax-2
     
     wK = zeros(wRKNumberOfIterations);
     
     for wRank=1:wRKNumberOfIterations
         
-        wSumK = zeros(wRKNumberOfIterations,1);
+        wSumBrsPerKs = zeros(wRKNumberOfIterations,1);
         
         for h=1:wRank-1
-            wSumK(:) = wSumK(:) + wBrs(wRank,h).*wK(:,h);
+            wSumBrsPerKs(:) = wSumBrsPerKs(:) + wBrs(wRank,h).*wK(:,h);
         end
         
-        wXp(:,wRank) = oX(:,n+wMatlabIndexBias) + iSampleTime*(wSumK);
+        wXp(:,wRank) = oX(:,n+wMatlabIndexBias) + iSampleTime*wAr(wRank)*(wSumBrsPerKs);
         wUp(:,wRank) = iInput(n+wMatlabIndexBias);
         wK(:,wRank)  = wA*wXp(:,wRank) + wB*wUp(:,wRank);
         
     end
     
-    wSumK = zeros(wRKNumberOfIterations,1);
+    wSumCrPerKr = zeros(wRKNumberOfIterations,1);
     for h=1:wRKNumberOfIterations
-        wSumK(h) = wK(h,:)*iCr(:);
+        wSumCrPerKr(h) = wK(h,:)*iCr(:);
     end
     
-    oX(:,n+wMatlabIndexBias+1) = oX(:,n+wMatlabIndexBias) + iSampleTime*(wSumK);
+    oX(:,n+wMatlabIndexBias+1) = oX(:,n+wMatlabIndexBias) + iSampleTime*(wSumCrPerKr);
     
 end
 
